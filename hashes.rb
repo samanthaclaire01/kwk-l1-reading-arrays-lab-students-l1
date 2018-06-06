547554c18dbfd838f87908590c742199cd95df94
Samantha Cassity <cassityclaire@gmail.com>
2018-06-06 22:05:31 +0000

    automatically backed up by learn

--- hashes.rb
+++ hashes.rb
@@ -18,9 +18,9 @@ celeb_crush = {}
 celeb_crush ["Emi"] = "Grant Gustin"
 puts celeb_crush
 class_celeb_crushes = {"Natalia" => "Timothee Chalamet", "Maya" => "Michael B. Jordan", "Amira" => "Cole Sprouse"}
-puts class_celeb_crushes
-puts class_celeb_crushes.values
-puts class_celeb_crushes.keys
+# puts class_celeb_crushes
+# puts class_celeb_crushes.values
+# puts class_celeb_crushes.keys
 
 class_celeb_crushes.each do |names,crushes|
 puts "#{names} crush is #{crushes}"
