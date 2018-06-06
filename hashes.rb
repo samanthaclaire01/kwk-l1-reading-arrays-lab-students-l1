0efaaa4ae8691657152c6230a33a8f0348e90bbf
Samantha Cassity <cassityclaire@gmail.com>
2018-06-06 21:49:31 +0000

    automatically backed up by learn

--- hashes.rb
+++ hashes.rb
@@ -1,13 +1,13 @@
 
-characteristics =["Neat Freak", "Jokester", "Free Spirit"]  #value
-character_names = ["Monica", "Joey", "Phoebe"] #key
+# characteristics =["Neat Freak", "Jokester", "Free Spirit"]  #value
+# character_names = ["Monica", "Joey", "Phoebe"] #key
 
-friends_characters = {}
+# friends_characters = {}
 
-index = 0
+# index = 0
 
-character_names.each do |name|
- friends_characters [name] = characteristics[index]
- index += 1
- end 
- puts friends_characters 
+# character_names.each do |name|
+# friends_characters [name] = characteristics[index]
+# index += 1
+# end 
+# puts friends_characters 
