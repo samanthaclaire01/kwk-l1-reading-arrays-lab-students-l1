57637f21f485e0f2825cd0e0a4a31bb4c18c5b11
Samantha Cassity <cassityclaire@gmail.com>
2018-06-06 21:48:05 +0000

    automatically backed up by learn

--- hashes.rb
+++ hashes.rb
@@ -1,1 +1,13 @@
-#
+
+characteristics =["Neat Freak", "Jokester", "Free Spirit"]  #value
+character_names = ["Monica", "Joey", "Phoebe"] #key
+
+friends_characters = {}
+
+index = 0
+
+character_names.each do |name|
+ friends_characters [name] = characteristics[index]
+ index += 1
+ end 
+ friends_characters 
