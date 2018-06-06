50c6eac1b8ba7ab532b697613b3db5f0e7bc2dcb
Samantha Cassity <cassityclaire@gmail.com>
2018-06-06 22:00:25 +0000

    automatically backed up by learn

--- hashes.rb
+++ hashes.rb
@@ -18,5 +18,6 @@ celeb_crush = {}
 celeb_crush ["Emi"] = "Grant Gustin"
 puts celeb_crush
 class_celeb_crushes = {"Natalia" => "Timothee Chalamet", "Maya" => "Michael B. Jordan", "Amira" => "Cole Sprouse"}
-
-puts class_celeb_crushes.values 
+puts class_celeb_crushes
+puts class_celeb_crushes.values
+puts class_celeb_crushes.keys
